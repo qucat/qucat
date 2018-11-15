50f4885724bb3cb75d9d4abc5ccaf527db8ef9ab
mgely <mario.f.gely@gmail.com>
2018-11-15 10:23:04 +0100

    Minimum Q added
    Modes which have a quality factor lower than the keyword argument Q_min (=1 by default) are discarded. Otherwise we get zeros of the admittance with dissipation rates near, zero and sometimes negative freqs and huge dissipation rates. These correspond to "rigid modes"

--- core.py
+++ core.py
@@ -24,15 +24,17 @@ def admittance(circuit):
     elif type(circuit) == lcapy.oneport.R:
         return 1/sp.Symbol(circuit.args[0],real=True)
 
+
 class Bbox(object):
     '''
     Given a circuit, the Bbox allows one to draw the circuit, and compute numerically
     and analytically (not yet) the frequency, dissipation rate and anharmonicity
     of the circuits different modes.
     '''
-    def __init__(self, circuit, L_J = "L_J"):
+    def __init__(self, circuit, L_J = "L_J", Q_min = 1.):
         self.circuit = circuit#.simplify()
         self.L_J = L_J
+        self.Q_min = Q_min
         
         Y = admittance(self.circuit)
         self.all_circuit_elements = [str(x) for x in list(Y.free_symbols)]
@@ -41,10 +43,10 @@ class Bbox(object):
         Y_numer = sp.numer(sp.together(Y))
         Y_poly = sp.collect(sp.expand(Y_numer),sp.Symbol('w'))
         self.Y_poly = Y_poly
-        Y_poly_order = sp.polys.polytools.degree(Y_poly,gen = sp.Symbol('w'))
-        Y_poly_coeffs = [Y_poly.coeff(sp.Symbol('w'),n) for n in range(Y_poly_order+1)[::-1]]
+        self.Y_poly_order = sp.polys.polytools.degree(Y_poly,gen = sp.Symbol('w'))
+        Y_poly_coeffs = [Y_poly.coeff(sp.Symbol('w'),n) for n in range(self.Y_poly_order+1)[::-1]]
         self.Y_poly_num = sp.utilities.lambdify([elt for elt in self.all_circuit_elements],Y_poly_coeffs,"numpy")
-        self.N_modes = int(math.ceil((Y_poly_order)/2))
+        self.N_modes = int(self.Y_poly_order/2)
         self.dY = sp.diff(Y,sp.Symbol('w'))
         self.dY_num = sp.utilities.lambdify([elt for elt in self.all_circuit_elements+["w"]],self.dY,"numpy")
     
@@ -93,15 +95,16 @@ class Bbox(object):
         '''
         args = [circuit_parameters[key] for key in self.all_circuit_elements]
         ws_cpx = np.roots(self.Y_poly_num(*args))
-        positive_sols = np.argwhere((np.real(ws_cpx)>=0.)&(np.imag(ws_cpx)>=0.))
+
+        # take only roots with a positive real part (i.e. freq) 
+        # and imaginary part (i.e. dissipation)
+        # and significant Q factors
+        positive_sols = np.argwhere((np.real(ws_cpx)>=0.)&(np.imag(ws_cpx)>=0.)&(np.real(ws_cpx)>self.Q_min*np.imag(ws_cpx)))
         ws_cpx=ws_cpx[positive_sols][:,0]
-        while True:
-            if len(ws_cpx)<self.N_modes:
-                ws_cpx=np.concatenate((ws_cpx,[0.]))
-            else:
-                break
         
         ws = np.real(ws_cpx)
+
+        # Sort solutions with increasing frequency
         increasing_frequencies = np.argsort(ws)
         ws = ws[increasing_frequencies]
         ks = np.imag(ws_cpx)[increasing_frequencies]
@@ -138,3 +141,28 @@ class Bbox(object):
             return np.transpose(ret,(2,1,0))
         else:
             "Can only iterate on one variable"
+
+if __name__ == '__main__':
+    b_cQED = Bbox(L('L_J') | C('C')| R('R_J') | (C('Cc')+(C('Cr')|L('Lr')|(C('Cf')+R('R_50')))))
+    flux = np.linspace(0.,0.4,103)
+    L_J_list = 7e-9/abs(np.cos(np.pi*flux))
+    to_plot =  np.array(b_cQED.normalmodes({
+    'L_J':L_J_list,
+    'C':100e-15,
+    'Cc':10e-15,
+    'Cf':1e-15,
+    'R_50':50.,
+    'Lr':10e-9,
+    'Cr':100e-15,
+    'R_J':1e6,
+    'R_r':1e5
+    }))
+    fig,axarr = plt.subplots(3,1,figsize=(6,9),sharex=True)
+    for i,ax in enumerate(axarr):
+        for j in range(b_cQED.N_modes):
+            axarr[i].plot(flux,to_plot[j,i])
+    axarr[0].set_ylabel("Mode frequency")
+    axarr[1].set_ylabel("Mode dissipation")
+    axarr[2].set_ylabel("Mode anharmonicity")
+    axarr[2].set_xlabel("$\phi/\phi_0$")
+    plt.show()
