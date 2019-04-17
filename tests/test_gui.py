f28d643288cb824dcc2a49bf478db2362141346a
mgely <mario.f.gely@gmail.com>
2019-04-17 14:40:19 +0200

    individual tests work but not multiple

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -3,8 +3,22 @@ import os
 import shutil
 from Qcircuits.src._gui import GuiWindow
 import inspect
+import tkinter as tk
+from time import sleep
 
-class ManualTesting(unittest.TestCase):
+class GuiTestingHandler(unittest.TestCase):
+
+    def close_gui(self):
+        while True:
+            try:
+                self.gui.master.destroy()
+            except Exception:#tk.TclError:
+                # The gui is no longer open
+                return
+
+    
+
+class ManualTesting(GuiTestingHandler):
 
     def get_netlist_filename(self):
         curframe = inspect.currentframe()
@@ -29,70 +43,69 @@ class ManualTesting(unittest.TestCase):
         return contents
 
 
-class GuiTesting(unittest.TestCase):
+class AutomaticTesting(GuiTestingHandler):
 
-    def launch_gui_testing(self,exclude = None):
-        folder = self.get_folder_name()
+    def launch_gui_testing(self,exclude = None, force_build = False):
+        self.set_folder_name()
+        self.set_file_names()
+        self.exclude = exclude
+        self.force_build = force_build
 
-        if not self.already_built(folder):
-            self.gui_build_test(folder,exclude)
+        if not self.already_built():
+            self.gui_build_test()
 
-        init = os.path.join(folder,'initial_netlist.txt')
-        events = os.path.join(folder,'events.txt')
-        self.run_events(folder,init,events)
+        self.run_events()
 
-        with open(os.path.join(folder,'final_netlist.txt'),'r') as f:
+        with open(self.final_expected,'r') as f:
             final_expected = f.read()
-        with open(os.path.join(folder,'final_after_events_netlist.txt'),'r') as f:
+        with open(self.final_after_events,'r') as f:
             final_after_events = f.read()
             
         self.assertEqual(final_expected,final_after_events)
 
-    def get_folder_name(self):
+    def set_file_names(self):
+        self.init = os.path.join(self.folder,'initial_netlist.txt')
+        self.events = os.path.join(self.folder,'events.txt')
+        self.final_expected = os.path.join(self.folder,'final_netlist.txt')
+        self.final_after_events = os.path.join(self.folder,'final_after_events_netlist.txt')
+
+    def set_folder_name(self):
         curframe = inspect.currentframe()
         calframe = inspect.getouterframes(curframe, 2)
         calling_function_name = calframe[2][3]
                 
-        folder = os.path.join(\
+        self.folder = os.path.join(\
             os.path.dirname(__file__),\
             ".gui_testing_files",\
             calling_function_name)
-        return folder
 
-    def run_events(self,folder,  init, events):
-        init = os.path.join(folder,'initial_netlist.txt')
-        final_after_events = os.path.join(folder,'final_after_events_netlist.txt')
-        events = os.path.join(folder,'events.txt')
+    def run_events(self):
 
-        shutil.copyfile(init,final_after_events)
-        gui = GuiWindow(final_after_events, _unittesting = True)
-        with open(events,'r') as f:
+        shutil.copyfile(self.init,self.final_after_events)
+        self.gui = GuiWindow(self.final_after_events, _unittesting = True)
+        with open(self.events,'r') as f:
             lines = f.readlines()
             for l in lines:
-                exec('gui.canvas.event_generate('+l+')', globals(), locals())
-        gui.destroy()
+                self.gui.canvas.focus_set()
+                exec('self.gui.canvas.event_generate('+l+')', globals(), locals())
+                self.gui.update()
+        self.close_gui()
 
-
-
-    def gui_build_test(self,folder,exclude):
-        init = os.path.join(folder,'initial_netlist.txt')
-        final = os.path.join(folder,'final_netlist.txt')
-        events = os.path.join(folder,'events.txt')
+    def gui_build_test(self):
 
         print("Build initial circuit")
-        GuiWindow(init)
-        shutil.copyfile(init,final)
+        GuiWindow(self.init)
+        shutil.copyfile(self.init,self.final_expected)
         print("Build final circuit")
-        GuiWindow(final, _track_events_to = events)
+        GuiWindow(self.final_expected, _track_events_to = self.events)
 
-        self.remove_exluded_events(folder,exclude)
+        self.remove_exluded_events()
 
-    def remove_exluded_events(self,folder,exclude):
-        if exclude is not None:
-            events = os.path.join(folder,'events.txt')
+    def remove_exluded_events(self):
+        if self.exclude is not None:
             temp = os.path.join(folder,'temp.txt')
-            shutil.copyfile(events,temp)
-            with open(events,'w') as to_write:
+            shutil.copyfile(self.events,temp)
+            with open(self.events,'w') as to_write:
                 with open(temp,'r') as to_read:
                     lines = to_read.readlines()
                     for l in lines:
@@ -104,23 +117,28 @@ class GuiTesting(unittest.TestCase):
                             to_write.write(l)
             os.remove(temp)
 
-
-    def already_built(self,folder):
+    def already_built(self):
         try:
-            os.mkdir(folder)
+            os.mkdir(self.folder)
             return False
         except FileExistsError:
             pass
         
-        for filename in [
-            'initial_netlist.txt',
-            'final_netlist.txt',
-            'events.txt']:
+        for filename in [self.init,self.final_expected,self.events]:
             try:
-                with open(os.path.join(folder,filename)):
+                with open(filename,'r'):
                     pass
             except FileNotFoundError:
                 return False 
+
+        if self.force_build:
+            for filename in [self.init,self.final_expected,self.events,self.final_after_events]:
+                try:
+                    os.remove(filename)
+                except FileNotFoundError:
+                    pass 
+            return False
+            
         return True
 
 
@@ -128,25 +146,31 @@ class TestOpening(ManualTesting):
 
     def test_if_opening_blank_test_throws_error(self):
         filename = self.write_netlist_file('')
-        gui = GuiWindow(filename, _unittesting = True)
-        gui.destroy()
+        self.gui = GuiWindow(filename, _unittesting = True)
+        self.close_gui()
         self.assertEqual('',self.read_netlist_file())
 
-class TestMovingComponentsAround(GuiTesting):
+class TestMovingComponentsAround(AutomaticTesting):
 
-    def test_moving_capacitor_horizontally(self):
-        self.launch_gui_testing()
+    # def test_moving_capacitor_horizontally(self):
+    #     self.launch_gui_testing()
 
     def test_rotating_capacitor(self):
         self.launch_gui_testing()
 
-    def test_moving_capacitor_twice(self):
+    def test_rotating_ground(self):
         self.launch_gui_testing()
 
-    def test_moving_parallel_RLCJG(self):
-        self.launch_gui_testing()
+
+    # def test_moving_capacitor_twice(self):
+    #     self.launch_gui_testing()
+
+    # def test_moving_parallel_RLCJG(self):
+    #     self.launch_gui_testing()
 
 if __name__ == "__main__":
-    unittest.main()
+    # unittest.main()
     # unittest.main(defaultTest='TestMovingComponentsAround')
+    unittest.main(defaultTest='TestOpening.test_if_opening_blank_test_throws_error')
     # unittest.main(defaultTest='TestMovingComponentsAround.test_rotating_capacitor')
+    # unittest.main(defaultTest='TestMovingComponentsAround.test_rotating_ground')
