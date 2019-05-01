0055e44125b2bc10c901288d89f40ebfa907b7da
mgely <mario.f.gely@gmail.com>
2019-05-01 20:31:24 +0200

    two tutorials completed

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -7,7 +7,11 @@ class TestTutorials(TestCaseAppended):
     def run_tutorial(self,notebook_name):
         var_dict = run_notebook(notebook_name)
         self.assertFalse(isinstance(var_dict,str),msg=var_dict)
-        rmtree('circuits')
+        try:
+            rmtree('circuits')
+        except FileNotFoundError:
+            # no folder was created
+            pass
         return var_dict
 
 
@@ -15,7 +19,9 @@ class TestBasics(TestTutorials):
 
     def test_transmon_LC_GUI(self):
         var_dict = self.run_tutorial('transmon_LC_GUI.ipynb')
-        self.assertArrayRelativelyClose(var_dict['w_10'],[4.98333767e+09, 5.03291805e+09])
+
+    def test_transmon_LC_programmatically(self):
+        var_dict = self.run_tutorial('transmon_LC_programmatically.ipynb')
         
 if __name__ == "__main__":
     unittest.main()
