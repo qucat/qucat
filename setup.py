8c50878f5b61dea1d4ed2a8094e231c21eb3bf45
Mario Gely - TNW <mfgely@tudelft.net>
2019-05-08 11:30:24 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qucat",
+    version="0.1",
+    author="Mario Gely",
+    author_email="mario.f.gely@gmail.com",
+    description="QUantum Circuit Analysis Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mgely/qucat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
