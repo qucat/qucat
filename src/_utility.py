22d8aad2d75067bd3dfd87835f8f75afe6a15a65
Mario Gely <mario.f.gely@gmail.com>
2019-05-09 11:34:17 +0200

    debugged pretty float printing

--- src/_utility.py
+++ src/_utility.py
@@ -64,73 +64,74 @@ def vectorize(func_to_evaluate):
             return np.vectorize(lambda w_single: func_to_evaluate(self,w_single,**kwargs))(w)
     return wrapper_vectorize
 
+def get_exponent_3(value):
+    value = np.absolute(value)
+    exponent = floor(np.log10(value))
+    exponent_3 = exponent-(exponent % 3)
+    return exponent_3
+
+def exponent_3_to_string(value,exponent_3,use_power_10,use_unicode):
+    value = np.absolute(value)
+    if use_power_10 or value >= 1e15 or value < 1e-18:
+        if exponent_3 == 0:
+            exponent_part = ''
+        else:
+            exponent_part = r'e%d' % exponent_3
+    else:
+        if use_unicode:
+            exponent_part = ' '+exponent_to_letter_unicode[exponent_3]
+        else:
+            exponent_part = ' '+exponent_to_letter[exponent_3]
+    return exponent_part
+
+def get_float_part(v,exponent_3,maximum_info):
+    if v == 0:
+        return '','0'
+    if v < 0:
+        sign = '-'
+        v *= -1
+    elif v > 0:
+        sign = ''
+    float_part = v/(10**exponent_3)
+
+    if maximum_info == False:
+        if float_part >= 100.:
+            float_part = "%2.0f" % (float_part)
+        elif float_part >= 10.:
+            float_part = "%1.1f" % (float_part)
+        else:
+            float_part = "%0.2f" % (float_part)
+        # remove trailing 0s or .
+        while float_part[-1]=="0":
+            float_part = float_part[:-1]
+        if float_part[-1]==".":
+            float_part = float_part[:-1]
+
+    else:
+        if float_part-float("%.6f"%float_part) != 0:
+            # if there is a digit beyond digit 6
+            float_part ="%.6f.."%float_part
+        else:
+            float_part ="%.6f"%float_part
+            while float_part[-1]=="0":
+                float_part = float_part[:-1]
+            if float_part[-1]==".":
+                float_part = float_part[:-1]
+    return [sign,float_part]
+
 
 def pretty_value(v,is_complex = True, use_power_10=False, use_unicode=True, maximum_info = False):
 
     if v == 0:
         return '0'
 
-    def get_exponent_3(value):
-        value = np.absolute(value)
-        exponent = floor(np.log10(value))
-        exponent_3 = exponent-(exponent % 3)
-        return exponent_3
-    
-    def exponent_3_to_string(value,exponent_3):
-        value = np.absolute(value)
-        if use_power_10 or value >= 1e15 or value < 1e-18:
-            if exponent_3 == 0:
-                exponent_part = ''
-            else:
-                exponent_part = r'e%d' % exponent_3
-        else:
-            if use_unicode:
-                exponent_part = ' '+exponent_to_letter_unicode[exponent_3]
-            else:
-                exponent_part = ' '+exponent_to_letter[exponent_3]
-        return exponent_part
-    
-    def get_float_part(v,exponent_3):
-        if v == 0:
-            return '','0'
-        if v < 0:
-            sign = '-'
-            v *= -1
-        elif v > 0:
-            sign = ''
-        float_part = v/(10**exponent_3)
-
-        if maximum_info == False:
-            if float_part >= 100.:
-                float_part = "%.0f" % (float_part)
-            elif float_part >= 10.:
-                float_part = "%.1f" % (float_part)
-            else:
-                float_part = "%.2f" % (float_part)
-            # remove trailing 0s or .
-            while float_part[-1]=="0":
-                float_part = float_part[:-1]
-            if float_part[-1]==".":
-                float_part = float_part[:-1]
-
-        else:
-            if float_part-float("%.6f"%float_part) != 0:
-                # if there is a digit beyond digit 6
-                float_part ="%.6f.."%float_part
-            else:
-                float_part ="%.6f"%float_part
-                while float_part[-1]=="0":
-                    float_part = float_part[:-1]
-                if float_part[-1]==".":
-                    float_part = float_part[:-1]
-        return [sign,float_part]
 
     if is_complex:
         exp3 = get_exponent_3(max(np.absolute(np.real(v)),np.absolute(np.imag(v))))
-        exponent = exponent_3_to_string(max(np.absolute(np.real(v)),np.absolute(np.imag(v))),exp3)
+        exponent = exponent_3_to_string(max(np.absolute(np.real(v)),np.absolute(np.imag(v))),exp3,use_power_10,use_unicode)
 
-        sign_r, numbers_r = get_float_part(np.real(v),exp3)
-        sign_i, numbers_i = get_float_part(np.imag(v),exp3)
+        sign_r, numbers_r = get_float_part(np.real(v),exp3,maximum_info)
+        sign_i, numbers_i = get_float_part(np.imag(v),exp3,maximum_info)
 
         to_return = ''
         if numbers_r.replace('0','').replace('.','')!='':
@@ -145,8 +146,8 @@ def pretty_value(v,is_complex = True, use_power_10=False, use_unicode=True, maxi
     else:
         v = np.real(v)
         exp3 = get_exponent_3(v)
-        exponent = exponent_3_to_string(v,exp3)
-        sign, numbers = get_float_part(v,exp3)
+        exponent = exponent_3_to_string(v,exp3,use_power_10,use_unicode)
+        sign, numbers = get_float_part(v,exp3,maximum_info)
         return sign+numbers+exponent
 
 def shift(to_shift,shift):
@@ -159,22 +160,23 @@ def to_string(unit,label,value, use_unicode=True, maximum_info = False):
     if unit is not None:
         if not use_unicode:
             unit = unit.replace(u"\u03A9", 'Ohm')
-
+            unit = unit.replace(u'\u03bc', 'u')
+                
     if label is None:
         s = ''
     else:
         s = label
-
         if value is not None:
             s+='='
 
     if value is not None:
-        s+= pretty_value(
-            value, use_unicode=use_unicode,maximum_info=maximum_info)
+        s+= pretty_value(value, use_unicode=use_unicode,maximum_info=maximum_info)
 
         if unit is not None:
             s+=unit
     return s
 
 if __name__ == "__main__":
-    print(pretty_value((1.1110564436955427e-09-3.4692726334931547e-10j),is_complex = True))
+    print(get_float_part(1.123e-13,-15,False))
+    print(get_float_part(1.123e-14,-15,False))
+    print(get_float_part(1.123e-15,-15,False))
