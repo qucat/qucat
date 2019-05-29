bafa4d0be8c18442905ca70df7c89e32552bd06a
Mario Gely - TNW <mfgely@tudelft.net>
2019-05-29 11:25:10 +0200

    New version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.dir_util import remove_tree
 
 setuptools.setup(
     name="qucat",
-    version="0.6.1",
+    version="0.7",
     author="Mario Gely",
     author_email="mario.f.gely@gmail.com",
     description="QUantum Circuit Analysis Tool",
