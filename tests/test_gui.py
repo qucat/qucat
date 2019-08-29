c3808226a4fe25e4a886c452bce3ab94ffd34d82
Mario Gely - TNW <mfgely@tudelft.net>
2019-08-29 17:44:53 +0200

    Update test_gui.py

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -187,10 +187,10 @@ class TestComponentCreation(AutomaticTesting):
     def test_building_overlapping_and_intersecting_wires(self):
         self.launch_gui_testing()
         
-    def test_building_capacitor(self):
-        self.launch_gui_testing()
     def test_building_resistor(self):
         self.launch_gui_testing(force_build=False)
+    def test_building_capacitor(self):
+        self.launch_gui_testing()
     def test_building_junction(self):
         self.launch_gui_testing()
     def test_building_ground(self):
