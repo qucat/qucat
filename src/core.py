Small errors corrected, but a sign error remains

src/core.py
@@ -947,6 +947,8 @@ class Qcircuit(object):
         """
         R_in = self.components[R_in_label]
         R_out = self.components[R_out_label]
+        R_in_value = R_in._get_value(**kwargs)
+        R_out_value = R_out._get_value(**kwargs)
 
         # Un-driven hamiltonian
         H_bare = self.hamiltonian(modes, taylor, excitations, **kwargs)
@@ -976,26 +978,29 @@ class Qcircuit(object):
 
         from qutip import steadystate, expect, commutator
 
-        rho = steadystate(H_bare_rot + H_drive, c_ops)
+        # Usin gth Scipy solver to avoid the
+        # "access violation reading 0xFFFFFFFFFFFFFFFF" error
+        # when using the mkl solver
+        # TODO: get to the bottom of this issue
+        rho = steadystate(H_bare_rot + H_drive, c_ops, solver="scipy")
 
         I_out_operator = 0
         for index, mode in enumerate(self.hamiltonian_modes):
             zpf = R_out.zpf(mode, "flux", **kwargs)
-            R_out_value = R_out._get_value(**kwargs)
             I_out_operator += (
-                -np.conj(zpf)
+                np.conj(zpf)
                 * 2
                 * 1j
                 / R_out_value
-                * commutator(self.a[index].dag(), 2 * pi * H_bare)
+                * commutator(2 * pi * H_bare, self.a[index].dag())
             )
 
         I_out = expect(rho, I_out_operator)
 
         if R_in == R_out:
-            return -1 + I_out / I_in
+            return -1 + (R_out_value * I_out) / (R_in_value * I_in)
         else:
-            return I_out / I_in
+            return (R_out_value * I_out) / (R_in_value * I_in)
 
     @refuse_vectorize_kwargs(exclude=["plot", "return_fig_ax"])
     def show(self, plot=True, return_fig_ax=False, **kwargs):
@@ -3003,17 +3008,13 @@ class R(Component):
     def _P_to_I(self, drive_power, drive_phase=0, power_unit="dBm"):
         r"""Turns a drive power and a phase into a current phasor.
 
-        Note
-        -----
-        0 phase will correspond to a sin.
-        So the phasor is actually |I|e^i([drive_phase]+pi/2)
         """
         if power_unit == "dBm":
             P_watt = np.power(10, ((drive_power - 30) / 10))
 
         I = np.sqrt(2 * P_watt / self._get_value())
 
-        return I * np.exp(1j * (drive_phase + pi / 2))
+        return I * np.exp(1j * drive_phase)
 
     def _drive_hamiltonian(self, I_in, **kwargs):
         r"""Computes the drive Hamiltonian.
@@ -3026,7 +3027,7 @@ class R(Component):
         a = self._circuit.a
         for index, mode in enumerate(self._circuit.hamiltonian_modes):
             eps = 1 / h * I_in * self.zpf(mode, "flux")
-            Hdr += -1j * (eps * a[index] - np.conj(eps) * a[index].dag())
+            Hdr += eps * a[index] + np.conj(eps) * a[index].dag()
 
         return Hdr
 

test.py
@@ -5,9 +5,23 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 from core import Network, GUI, J, L, C, R
 import numpy as np
 
-c = Network([C(0, 1, 100e-15), L(0, 1, 10e-9), C(1, 2, 10e-15), R(0, 2, 50, "p"),])
+Ci = 100e-15
+Li = 10e-9
+Ri = 1e6
+Z0 = 2e6
+
+
+def S11_ana(f):
+    w = f * 2 * np.pi
+    w0 = 1 / np.sqrt(Li * Ci)
+    ki = Z0 / (Ri + Z0) * (1 / Ri / Ci + 1 / Z0 / Ci)
+    Zin = Ri / (1 + 1j * w0 / ki * (w / w0 - w0 / w))
+    return (Zin - Z0) / (Zin + Z0)
+
+
+c = Network([C(0, 1, Ci), R(0, 1, Ri), L(0, 1, Li), R(0, 1, Z0, "p"),])
 f, k, _, _ = c.f_k_A_chi(pretty_print=True)
-freqs = np.linspace(f[0] - 3 * k[0], f[0] + 3 * k[0], 30)
+freqs = np.linspace(f[0] - 3 * k[0], f[0] + 3 * k[0], 31)
 S11 = []
 for fd in freqs:
     S11.append(
@@ -25,10 +39,10 @@ for fd in freqs:
             excitations=4,
         )
     )
-print(S11)
 
 import matplotlib.pyplot as plt
 
-plt.plot(freqs, np.real(S11))
-plt.plot(freqs, np.imag(S11))
+plt.plot(np.real(S11), np.imag(S11), label="qucat")
+plt.plot(np.real(S11_ana(freqs)), np.imag(S11_ana(freqs)), label="analytical")
+plt.legend()
 plt.show()