4e0bc3c79fed31862ac54ead9c3f811634756d48
Mario Gely <mario.f.gely@gmail.com>
2021-04-24 11:05:53 +0100

    Revert "Don't allow two components having the same label"
    This reverts commit 80b759e8342a0393b54abc898da93ab6a5e4475e.

--- src/core.py
+++ src/core.py
@@ -2109,8 +2109,9 @@ class Component(Circuit):
 
         if self.value is None and self.label not in ['', ' ', 'None', None]:
             if self.label in self._circuit._no_value_components:
-                raise ValueError(
-                    "Two components may not have the same label '%s'" % self.label)
+                # raise ValueError(
+                #     "Two components may not have the same name %s" % self.label)
+                pass
             else:
                 self._circuit._no_value_components.append(self.label)
 
